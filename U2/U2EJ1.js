33fbcb05ac53b4cd79c13c12ad7230403601bc64
jpuig3-crypto <jpuig3@uoc.edu>
2026-05-03 18:14:27 +0300

    U2EJ1.js

--- U2/U2EJ1.js
+++ U2/U2EJ1.js
@@ -31,10 +31,21 @@ class Car {
 }
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
+const randomCar = {
+  brand: "Ford",
+  model: "Mustang Boss 429",
+  displacement: 7000,
+  horsePower: 375,
+  year: 1969,
+};
 
 
+const myCar1 = new Car("Ford", "Mustang Boss 429", 7000, 375, 1969);
+const myCar2 = new Car("BMW", "520d", 1980, 136, 2002);
+const myCar3 = new Car("Mercedes-Benz", "280 SLC", 2746, 136, 1979);
+const cars = [myCar1, myCar2, myCar3];
 
-
+console.log(cars);
 
 /**
  * TEST
